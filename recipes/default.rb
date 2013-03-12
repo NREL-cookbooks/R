009800a49a5309bca2aab855c63d09130bfef6fb
Nicholas Long <nicholas.long@nrel.gov>
2013-03-12 12:57:06 -0600

    hack to make R work.  need to update to adding new source to deb

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,23 @@
 if platform_family?("debian")
   bash "install R" do
     code <<-EOH
-        apt-get -f install r-base  -y
+      echo "deb http://cran.stat.ucla.edu/bin/linux/ubuntu precise/" >> /etc/apt/sources.list
+      apt-get update
+      apt-get -f install r-base r-base-dev -y --force-yes
     EOH
   end
+
+  not_if { ::File.exists?("/usr/bin/R") }
+end
+
+bash "install packages" do
+  cwd "/tmp"
+  code <<-EOH
+
+    wget http://cran.r-project.org/src/contrib/lhs_0.10.tar.gz
+    R CMD INSTALL lhs_0.10.tar.gz
+
+    wget http://cran.r-project.org/src/contrib/Rserve_0.6-8.tar.gz
+    R CMD INSTALL Rserve_0.6-8.tar.gz
+  EOH
 end
